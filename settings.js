03f2c9aa61913709d71d7702295b3d6bab4e81c4
storm-reborn <stormobieri@gmail.com>
2025-05-10 11:15:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'storm',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'storm', // Your name
+  ownerNumber: '2349037971109', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
